717bf3ad8e7a0ef99b69e58339f67baf6e680e5b
Joeri Sykora <joeri@sertik.net>
2023-05-05 17:26:36 +0200

    remove ffi changes

--- rust/bridge/shared/src/ffi/error.rs
+++ rust/bridge/shared/src/ffi/error.rs
@@ -10,7 +10,6 @@ use std::io::{Error as IoError, ErrorKind as IoErrorKind};
 use attest::hsm_enclave::Error as HsmEnclaveError;
 use attest::sgx_session::Error as SgxError;
 use device_transfer::Error as DeviceTransferError;
-use signal_grpc::Error as GrpcError;
 use libsignal_protocol::*;
 use signal_crypto::Error as SignalCryptoError;
 use signal_pin::Error as PinError;
@@ -26,7 +25,6 @@ use super::NullPointerError;
 pub enum SignalFfiError {
     Signal(SignalProtocolError),
     DeviceTransfer(DeviceTransferError),
-    Grpc(GrpcError),
     HsmEnclave(HsmEnclaveError),
     Sgx(SgxError),
     Pin(PinError),
@@ -49,9 +47,6 @@ impl fmt::Display for SignalFfiError {
             SignalFfiError::Signal(s) => write!(f, "{}", s),
             SignalFfiError::DeviceTransfer(c) => {
                 write!(f, "Device transfer operation failed: {}", c)
-            }
-            SignalFfiError::Grpc(e) => {
-                write!(f, "Grpc operation failed: {}", e)
             }
             SignalFfiError::HsmEnclave(e) => {
                 write!(f, "HSM enclave operation failed: {}", e)
@@ -90,12 +85,6 @@ impl From<SignalProtocolError> for SignalFfiError {
 impl From<DeviceTransferError> for SignalFfiError {
     fn from(e: DeviceTransferError) -> SignalFfiError {
         SignalFfiError::DeviceTransfer(e)
-    }
-}
-
-impl From<GrpcError> for SignalFfiError {
-    fn from(e: GrpcError) -> SignalFfiError {
-        SignalFfiError::Grpc(e)
     }
 }
 
